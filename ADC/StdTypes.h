b269907b117ec7ecf8e480a2fa3d07ed29296cc7
Marawan Azmy <marawan.azmy@hotmail.com>
2024-05-19 22:06:18 +0300

    .

---
+++ ADC/StdTypes.h
@@ -0,0 +1,19 @@
+/*
+ * StdTypes.h
+ *
+ *  Created on: 9 Apr 2024
+ *      Author: marmar
+ */
+
+#ifndef STDTYPES_H_
+#define STDTYPES_H_
+
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned long uint32;
+typedef float f32;
+typedef double f64;
+
+
+
+#endif /* STDTYPES_H_ */
